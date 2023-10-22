b7b357f9c1159d5830eec9f1bec3ad0468ebc4c5
Kaleryia Azarka <ferr.vector@gmail.com>
2023-10-22 18:10:44 +0300

    Task 3

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -5,15 +5,33 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-Console.WriteLine("Введите первое число");
-int x = Int32.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int y = Int32.Parse(Console.ReadLine());
-Console.WriteLine("Введите третье число");
-int z = Int32.Parse(Console.ReadLine());
 
-int max = x;
-if (y > max) max = y;
-if (z > max) max = z;
 
+
+// Console.WriteLine("Введите первое число");
+// int x = Int32.Parse(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int y = Int32.Parse(Console.ReadLine());
+// Console.WriteLine("Введите третье число");
+// int z = Int32.Parse(Console.ReadLine());
+
+// int max = x;
+// if (y > max) max = y;
+// if (z > max) max = z;
+
+// Console.WriteLine(max);
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int a = 15;
+int b = 29;
+int c = 48;
+
+int max = Max(a, b, c);
 Console.WriteLine(max);
